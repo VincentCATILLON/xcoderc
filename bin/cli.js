970f74189ac0b383a15b694903953784786c1761
Vincent Catillon <devotion95@gmail.com>
2019-04-26 14:44:29 +0200

    Add exit code and timeout option

--- bin/cli.js
+++ bin/cli.js
@@ -4,6 +4,7 @@
 
 'use strict';
 
+const emoji = require('node-emoji');
 const chalk = require('chalk');
 const args = require('args');
 
@@ -11,14 +12,22 @@ const lib = require('../lib').default;
 const {COMMAND, CONFIG_FILE_NAME, BINARY_NAME} = require('../lib/const');
 const {UnknownCommandError} = require('../lib/helpers/errors');
 
+let exitCode;
+
 function handler(command) {
-  lib(command)
-    .catch(e => {
+  return lib(command)
+    .then(function() {
+      exitCode = 0;
+    })
+    .catch(function(e) {
       console.error(chalk.red(e.message));
 
       if (e instanceof UnknownCommandError) {
         console.log("\n");
+        exitCode = 127;
         args.showHelp();
+      } else {
+        exitCode = 1;
       }
     });
 }
@@ -26,15 +35,39 @@ function handler(command) {
 args
   .command('check', `Check the locked version in ${CONFIG_FILE_NAME} file with your current Xcode version. (default)`, handler)
   .command('init', `Creates a ${CONFIG_FILE_NAME} file with your current Xcode version`, handler)
-  .command('use', `Switch to the Xcode instance matching with your ${CONFIG_FILE_NAME} locked version.`, handler);
+  .command('use', `Switch to the Xcode instance matching with your ${CONFIG_FILE_NAME} locked version.`, handler)
+  .option('timeout', `Set execution timeout.`, 15000);
 
 const [,, command] = process.argv;
 
-args.parse(process.argv, {
+const flags = args.parse(process.argv, {
   name: BINARY_NAME,
-  mainColor: 'green'
+  mainColor: 'green',
+  exit: {
+    help: false,
+    version: false
+  }
 });
 
-if (!command || !Object.values(COMMAND).includes(command)) {
+if (['version', 'help'].includes(command)) {
+  exitCode = 0;
+} else if (Object.values(COMMAND).includes(command)) {
+  // Do nothing
+} else if (!command || command.slice(0, 1) === '-') {
+  handler();
+} else {
   handler(command);
 }
+
+let executionTime = 0;
+
+setInterval(function() {
+  executionTime += 100;
+  if (executionTime >= flags.timeout) {
+    console.error(chalk.red(`${emoji.get('confused')} The script ended with timeout. Try to increase this value with '--timeout' option.`));
+    process.exit(124);
+  }
+  if (exitCode !== undefined) {
+    process.exit(exitCode);
+  }
+}, 100);
